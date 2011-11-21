5128fdb269dfc727015cde04d8af6dc6a99c82f3
Alok Simha <lukasimha@gmail.com>
2011-11-21 19:05:49 +0530

    <Shantala/Manohar/Luka> #722: Masking audits and logs which logged the patient phone number. Also masking clinic and clinician phone number to avoid dialing actual clinicians during symptom reporting flow

--- prod-dump/patient_mask.py
+++ prod-dump/patient_mask.py
@@ -23,4 +23,30 @@ for id in db:
 			del doc["_rev"]
 			doc["_id"] = prefix + "000000" + mobileNumber[ len(mobileNumber) - 4 : ] + "/0000"
 			db.create(doc)
+	if "documentType" in doc and doc["documentType"] == "CallLog":
+		doc["phoneNumber"] = "000000" + doc["phoneNumber"][ len(doc["phoneNumber"]) - 4 : ]
+		db[doc.id] = doc
+	if "documentType" in doc and doc["documentType"] == "IVRCallAudit":
+		doc["cid"] = "000000" + doc["cid"][ len(doc["cid"]) - 4 : ]
+		db[doc.id] = doc
+	if "documentType" in doc and doc["documentType"] == "Clinician":
+		doc["contactNumber"] = "000000" + doc["contactNumber"][ len(doc["contactNumber"]) - 4 : ]
+		if "alternateContactNumber" in doc and len(doc["alternateContactNumber"]) > 0:
+			doc["alternateContactNumber"] = "000000" + doc["alternateContactNumber"][ len(doc["alternateContactNumber"]) - 4 : ]
+		db[doc.id] = doc
+	if "documentType" in doc and doc["documentType"] == "Clinic":
+		doc["phone"] = "000000" + doc["phone"][ len(doc["phone"]) - 4 : ]
+		for clinicianContact in doc["clinicianContacts"]:
+			clinicianContact["phoneNumber"] = "000000" + clinicianContact["phoneNumber"][ len(clinicianContact["phoneNumber"]) - 4 : ]
+		db[doc.id] = doc
+
+
+
+db = couch['motech-ivr-kookoo']
+
+for id in db:
+	doc = db[id]
+	if "type" in doc and doc["type"] == "KookooCallDetailRecord":
+		doc["callDetailRecord"]["phoneNumber"] = "000000" + doc["callDetailRecord"]["phoneNumber"][ len(doc["callDetailRecord"]["phoneNumber"]) - 4 : ]
+		db[doc.id] = doc
 
