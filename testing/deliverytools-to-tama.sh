5bb8ee363fed3076bb15941501547f8bbd9b4289
unknown <vsingh@.corporate.thoughtworks.com>
2011-11-23 16:06:06 +0530

    Vivek Singh | Fixing the script

--- testing/deliverytools-to-tama.sh
+++ testing/deliverytools-to-tama.sh
@@ -1,3 +1,3 @@
-copy ../../motech-delivery-tools/motech-datetime-simulator/target/motech-datetime-simulator-0.3-SNAPSHOT.jar %1/webapps/tama/WEB-INF/lib
-copy ../../motech-delivery-tools/motech-job-handler-invoker/target/motech-job-handler-invoker-0.3-SNAPSHOT.jar %1/webapps/tama/WEB-INF/lib
-copy ../../motech-delivery-tools/motech-outbound-endpoint/target/motech-outbound-endpoint-0.3-SNAPSHOT.jar %1/webapps/tama/WEB-INF/lib
+cp ../../motech-delivery-tools/motech-datetime-simulator/target/motech-datetime-simulator-0.3-SNAPSHOT.jar %1/webapps/tama/WEB-INF/lib
+cp ../../motech-delivery-tools/motech-job-handler-invoker/target/motech-job-handler-invoker-0.3-SNAPSHOT.jar %1/webapps/tama/WEB-INF/lib
+cp ../../motech-delivery-tools/motech-outbound-endpoint/target/motech-outbound-endpoint-0.3-SNAPSHOT.jar %1/webapps/tama/WEB-INF/lib
