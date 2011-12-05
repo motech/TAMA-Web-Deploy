083849e3dafade4338209644ecbda8af7b6cee14
unknown <vsingh@.corporate.thoughtworks.com>
2011-12-05 17:54:54 +0530

    Vivek Singh | added scripts useful for testing

--- testing/deliverytools-to-tama.sh
+++ testing/deliverytools-to-tama.sh
@@ -1,3 +1,5 @@
+cp date.properties $1/webapps/tama/WEB-INF/classes
+cp placeholderPluginContext.xml $1/webapps/tama/WEB-INF/classes
 cp ../../motech-delivery-tools/motech-datetime-simulator/target/motech-datetime-simulator-0.3-SNAPSHOT.jar $1/webapps/tama/WEB-INF/lib
 cp ../../motech-delivery-tools/motech-job-handler-invoker/target/motech-job-handler-invoker-0.3-SNAPSHOT.jar $1/webapps/tama/WEB-INF/lib
 cp ../../motech-delivery-tools/motech-outbound-endpoint/target/motech-outbound-endpoint-0.3-SNAPSHOT.jar $1/webapps/tama/WEB-INF/lib
