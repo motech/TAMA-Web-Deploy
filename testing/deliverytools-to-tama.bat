44ad484be5a085080016b02eff7696e5b29aee7d
Alok Simha <lukasimha@gmail.com>
2011-12-29 09:30:02 +0530

    <Luka> #0000: Removing the testing tool files

--- testing/deliverytools-to-tama.bat
+++
@@ -1,5 +0,0 @@
-copy ivr.properties %1\webapps\tama\WEB-INF\classes
-copy placeholderPluginContext.xml %1\webapps\tama\WEB-INF\classes
-copy ..\..\motech-delivery-tools\motech-datetime-simulator\target\motech-datetime-simulator-0.3-SNAPSHOT.jar %1\webapps\tama\WEB-INF\lib
-copy ..\..\motech-delivery-tools\motech-job-handler-invoker\target\motech-job-handler-invoker-0.3-SNAPSHOT.jar %1\webapps\tama\WEB-INF\lib
-copy ..\..\motech-delivery-tools\motech-outbound-endpoint\target\motech-outbound-endpoint-0.3-SNAPSHOT.jar %1\webapps\tama\WEB-INF\lib
--- testing/deliverytools-to-tama.sh
+++
@@ -1,4 +0,0 @@
-cp placeholderPluginContext.xml $1/webapps/tama/WEB-INF/classes
-cp ../../motech-delivery-tools/motech-datetime-simulator/target/motech-datetime-simulator-0.3-SNAPSHOT.jar $1/webapps/tama/WEB-INF/lib
-cp ../../motech-delivery-tools/motech-job-handler-invoker/target/motech-job-handler-invoker-0.3-SNAPSHOT.jar $1/webapps/tama/WEB-INF/lib
-cp ../../motech-delivery-tools/motech-outbound-endpoint/target/motech-outbound-endpoint-0.3-SNAPSHOT.jar $1/webapps/tama/WEB-INF/lib
