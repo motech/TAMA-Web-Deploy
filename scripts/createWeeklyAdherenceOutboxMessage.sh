180f8495fb4ebd5b7072819184460adabce2b3ff
BalajiN <balajin@thoughtworks.com>
2012-02-02 10:12:51 +0530

    BalajiN | #748 | Script to create dummy weekly adherence log message

---
+++ scripts/createWeeklyAdherenceOutboxMessage.sh
@@ -0,0 +1,2 @@
+#usage sh createWeeklyAdherenceOutboxMessage.sh id
+curl -X POST http://127.0.0.1:5984/motech-outbox -H "Content-Type: application/json" -d '{"partyId" : "'$1'","creationTime" : "2012-02-01T10:36:42.517+0000","expirationDate":"2212-02-07T18:30:00.000+0000","status":"PENDING",}'
