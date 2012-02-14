281a661a2f39a19b488bcba665d972c7d3b23f37
BalajiN <balajin@thoughtworks.com>
2012-02-14 15:39:24 +0530

    BalajiN, Preethi | #41 | Script now creates an outbox message of type AudioCommand instead of Audio

--- scripts/createWeeklyAdherenceOutboxMessage.sh
+++ scripts/createWeeklyAdherenceOutboxMessage.sh
@@ -2,4 +2,4 @@
 
 
 
-curl -X POST http://127.0.0.1:5984/motech-outbox -H "Content-Type: application/json" -d '{"partyId" : "'$1'","creationTime" : "2012-02-01T10:36:42.517+0000","expirationDate":"2212-02-07T18:30:00.000+0000","status":"PENDING","voiceMessageType":{"voiceMessageTypeName":"Audio","priority":"LOW","templateName":"temp","canBeSaved":"true","canBeReplayed":"true"}}'
+curl -X POST http://127.0.0.1:5984/motech-outbox -H "Content-Type: application/json" -d '{"partyId" : "'$1'","creationTime" : "2012-02-01T10:36:42.517+0000","expirationDate":"2212-02-07T18:30:00.000+0000","status":"PENDING","voiceMessageType":{"voiceMessageTypeName":"AudioCommand","priority":"LOW","templateName":"temp","canBeSaved":"true","canBeReplayed":"true"}}'
