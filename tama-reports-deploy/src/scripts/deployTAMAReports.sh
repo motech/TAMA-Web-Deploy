b5fb66bc0a9ee8e4beffe660019b254a8482d3ed
Balaji Narain <balajin@thoughtworks.com>
2013-01-29 18:49:36 +0530

    BalajiN | #5 | Fixing bugs

--- tama-reports-deploy/src/scripts/deployTAMAReports.sh
+++ tama-reports-deploy/src/scripts/deployTAMAReports.sh
@@ -2,14 +2,14 @@
 BUILD_ENV=$1
 BUILD=$2
 WORKING_DIR=/tmp/deployReports
-DEPLOY_VERSION=1.0-SNAPSHOT
+DEPLOY_VERSION=$3
 HUDSON_JOB_NAME=tama-reporting
 
 rm -rf $WORKING_DIR
 mkdir $WORKING_DIR
 cd $WORKING_DIR
 
-wget 'http://old-ci.motechproject.org/view/tama/job/tama-reports-deploy/lastSuccessfulBuild/org.motechproject.tama.reports$tama-reports-deploy/artifact/org.motechproject.tama.reports/tama-reports-deploy/'$DEPLOY_VERSION'/tama-reports-deploy-'$DEPLOY_VERSION'.jar'
+wget 'http://192.168.42.26/view/tama/job/tama-reports-deploy/lastSuccessfulBuild/org.motechproject.tama.reports$tama-reports-deploy/artifact/org.motechproject.tama.reports/tama-reports-deploy/'$DEPLOY_VERSION'/tama-reports-deploy-'$DEPLOY_VERSION'.jar'
 unzip tama-reports-deploy-$DEPLOY_VERSION.jar
-rm -rf tama-reports-deploy-$DEPLOY_VERSION.jar
-sudo ant deploy.from.hudson.with.reset.db -Denv=$BUILD_ENV -lib . -Dbuild=$BUILD
+rm -rf tama-reports-deploy-$DEPLOY_VERSION.jar      
+sudo ant deploy.from.hudson.with.reset.db -f deploy.xml -Denv=$BUILD_ENV -lib . -Dbuild=$BUILD
