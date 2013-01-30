a16536881d907ff628c45f339885589d43719c39
Balaji Narain <balajin@thoughtworks.com>
2013-01-30 10:15:16 +0530

    BalajiN | #5 | Newline correction

--- tama-reports-deploy/src/scripts/deployTAMAReports.sh
+++ tama-reports-deploy/src/scripts/deployTAMAReports.sh
@@ -11,5 +11,5 @@ cd $WORKING_DIR
 
 wget 'http://192.168.42.26/view/tama/job/tama-reports-deploy/lastSuccessfulBuild/org.motechproject.tama.reports$tama-reports-deploy/artifact/org.motechproject.tama.reports/tama-reports-deploy/'$DEPLOY_VERSION'/tama-reports-deploy-'$DEPLOY_VERSION'.jar'
 unzip tama-reports-deploy-$DEPLOY_VERSION.jar
-rm -rf tama-reports-deploy-$DEPLOY_VERSION.jar      
+rm -rf tama-reports-deploy-$DEPLOY_VERSION.jar
 sudo ant deploy.from.hudson.with.reset.db -f deploy.xml -Denv=$BUILD_ENV -lib . -Dbuild=$BUILD
