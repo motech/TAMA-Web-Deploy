dc108dc05d876eef256671954ca3febf77dd3540
Balaji Narain <balajin@thoughtworks.com>
2013-04-03 11:22:33 +0530

    BalajiN | #0 | Updating cache tools to cache all audio files

--- scripts/cache.sh
+++ scripts/cache.sh
@@ -1,34 +1,25 @@
 #!/bin/sh
 
-if [ -z "$1" ]
+if [ ! "$#" -eq "2" ]
 then
-   echo "Usage: ./cache.sh <path_to_TAMA-Web-Audio_folder>"
-   echo "Example: ./cache.sh /home/motech/TAMA-Web-Audio"
+   echo "Usage: ./cache.sh <path_to_TAMA-Web-Audio_folder> API_KEY"
+   echo "Example: ./cache.sh /home/motech/TAMA-Web-Audio apikey"
    exit 3
 fi
 
 KOOKOO_URL=http://kookoo.in/restkookoo/index.php/api/cache/audio
-KOOKOO_API_KEY=<your-kookoo-api-key>
+KOOKOO_API_KEY=$2
 TAMA_HOSTNAME=tama.showcase.thoughtworks.com
 
-echo "Processing english files"
+cd $1;
 
-for i in `find $1/en -name "*.wav" `; do 
- wave_file_name=`basename $i|tr [A-Z] [a-z]`
- echo "Deleting...en:$wave_file_name"
- curl $KOOKOO_URL/api_key/$KOOKOO_API_KEY/url/http:%5C%5C$TAMA_HOSTNAME%5Ctama%5Cwav%5Cstream%5Cen%5C$wave_file_name/delete/true
- echo "Uploading...en:$wave_file_name"
- curl -d "api_key=$KOOKOO_API_KEY&url=http://$TAMA_HOSTNAME/tama/wav/stream/en/$wave_file_name" $KOOKOO_URL
+for dir in `ls -p | grep "/" | tr -d /`; do
+ echo "Processing "$dir" files"
+ for i in `find $dir -name "*.wav" `; do
+  wave_file_name=`basename $i|tr [A-Z] [a-z]`
+  echo "Deleting..."$dir":"$wave_file_name"
+  curl $KOOKOO_URL/api_key/$KOOKOO_API_KEY/url/http:%5C%5C$TAMA_HOSTNAME%5Ctama%5Cwav%5Cstream%5C$dir%5C$wave_file_name/delete/true
+  echo "Uploading..."$dir":"$wave_file_name"
+  curl -d "api_key=$KOOKOO_API_KEY&url=http://$TAMA_HOSTNAME/tama/wav/stream/$dir/$wave_file_name" $KOOKOO_URL
+ done
 done
-
-echo "Processing marathi files"
-
-for i in `find $1/mr -name "*.wav" `; do 
- wave_file_name=`basename $i|tr [A-Z] [a-z]`
- echo "Deleting...mr:$wave_file_name"
- curl $KOOKOO_URL/api_key/$KOOKOO_API_KEY/url/http:%5C%5C$TAMA_HOSTNAME%5Ctama%5Cwav%5Cstream%5Cmr%5C$wave_file_name/delete/true
- echo "Uploading...mr:$wave_file_name"
- curl -d "api_key=$KOOKOO_API_KEY&url=http://$TAMA_HOSTNAME/tama/wav/stream/mr/$wave_file_name" $KOOKOO_URL 
-done
-
-echo "Done"
