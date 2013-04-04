9cd4e33b819f66f1c2a869553cf2f5fa927c63ae
Balaji Narain <balajin@thoughtworks.com>
2013-04-04 13:51:35 +0530

    Anoop,BalajiN | #0 | Fixing build box url

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -11,7 +11,7 @@ rm -rf $WORKING_DIR
 mkdir $WORKING_DIR
 cd $WORKING_DIR
 
-wget http://'$HUDSON_ADDRESS'/view/tama/job/$HUDSON_JOB_NAME/lastSuccessfulBuild/TAMA-Web-Deploy\$TAMA-Web-Deploy/artifact/TAMA-Web-Deploy/TAMA-Web-Deploy/$DEPLOY_VERSION/TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
+wget http://$HUDSON_ADDRESS/view/tama/job/$HUDSON_JOB_NAME/lastSuccessfulBuild/TAMA-Web-Deploy\$TAMA-Web-Deploy/artifact/TAMA-Web-Deploy/TAMA-Web-Deploy/$DEPLOY_VERSION/TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
 unzip TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
 rm -rf TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
 cd TAMA-Web-Deploy-$DEPLOY_VERSION && sudo ant $3 -f deploy.xml -Denv=$BUILD_ENV -Dbuild=$BUILD
