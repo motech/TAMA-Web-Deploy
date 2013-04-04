38d7703093d1eb96a36b29467f91131807d9f182
Balaji Narain <balajin@thoughtworks.com>
2013-04-04 12:58:47 +0530

    Anoop,BalajiN | #0 | Parameterize hudson address

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 BUILD_ENV=$1
 BUILD=$2
+HUDSON_ADDRESS=$4
 WORKING_DIR=/tmp/deploy
 DEPLOY_VERSION=0.1-SNAPSHOT
 DEPLOY_TARGET=$3
@@ -10,7 +11,7 @@ rm -rf $WORKING_DIR
 mkdir $WORKING_DIR
 cd $WORKING_DIR
 
-wget http://192.168.42.26/view/tama/job/$HUDSON_JOB_NAME/lastSuccessfulBuild/TAMA-Web-Deploy\$TAMA-Web-Deploy/artifact/TAMA-Web-Deploy/TAMA-Web-Deploy/$DEPLOY_VERSION/TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
+wget http://'$HUDSON_ADDRESS'/view/tama/job/$HUDSON_JOB_NAME/lastSuccessfulBuild/TAMA-Web-Deploy\$TAMA-Web-Deploy/artifact/TAMA-Web-Deploy/TAMA-Web-Deploy/$DEPLOY_VERSION/TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
 unzip TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
 rm -rf TAMA-Web-Deploy-$DEPLOY_VERSION-bin.zip
 cd TAMA-Web-Deploy-$DEPLOY_VERSION && sudo ant $3 -f deploy.xml -Denv=$BUILD_ENV -Dbuild=$BUILD
--- bootstrap/deployReports.sh
+++ bootstrap/deployReports.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 BUILD_ENV=$1
+HUDSON_ADDRESS=$4
 BUILD=$2
 WORKING_DIR=/tmp/deployReports
 DEPLOY_VERSION=1.0-SNAPSHOT
@@ -10,7 +11,7 @@ rm -rf $WORKING_DIR
 mkdir $WORKING_DIR
 cd $WORKING_DIR
 
-wget 'http://192.168.42.26/view/tama/job/'$HUDSON_JOB_NAME'/lastSuccessfulBuild/org.motechproject.tama.reports$tama-reports-deploy/artifact/org.motechproject.tama.reports/tama-reports-deploy/'$DEPLOY_VERSION'/tama-reports-deploy-'$DEPLOY_VERSION'.jar'
+wget 'http://'$HUDSON_ADDRESS'/view/tama/job/'$HUDSON_JOB_NAME'/lastSuccessfulBuild/org.motechproject.tama.reports$tama-reports-deploy/artifact/org.motechproject.tama.reports/tama-reports-deploy/'$DEPLOY_VERSION'/tama-reports-deploy-'$DEPLOY_VERSION'.jar'
 unzip tama-reports-deploy-$DEPLOY_VERSION.jar
 rm -rf tama-reports-deploy-$DEPLOY_VERSION.jar
 sudo ant $3 -f deploy.xml -Denv=$BUILD_ENV -lib . -Dbuild=$BUILD
