dea66c9019967cbb6f0153e8641fb98f99a437fd
Balaji Narain <balajin@thoughtworks.com>
2013-04-09 19:15:46 +0530

    BalajiN | #0 | Adding scripts to install couchdb and postgres with required versions

---
+++ scripts/version2/install_dependencies.sh
@@ -0,0 +1,22 @@
+#Vars
+PWD=/tmp/install
+
+#cwd
+rm -rf $PWD
+mkdir $PWD
+
+#Copy resources
+cp ./configuration.pp $PWD
+
+#Move in
+cd $PWD
+
+echo "Fetching resources..."
+wget https://raw.github.com/motech/motech-scm/master/bootstrap.sh
+
+echo "Installing..."
+sudo sh ./bootstrap.sh -c configuration.pp
+
+echo "Cleaning up..."
+rm -rf PWD
+echo "Done."
--- scripts/version2/main.sh
+++ scripts/version2/main.sh
@@ -1,2 +1,6 @@
 cd "$(dirname "$0")"
+echo "Migrating Jobs..."
 sh ./migrate_jobs.sh
+cd "$(dirname "$0")"
+echo "Installing dependencies..."
+sh ./install_dependencies.sh
