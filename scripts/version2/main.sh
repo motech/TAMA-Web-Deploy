8fa21e507db75e7bbde04b98c3a820d683bc4346
Balaji Narain <balajin@thoughtworks.com>
2013-04-17 11:06:12 +0530

    BalajiN | #0 | Caching audio files as a part of deployment

--- scripts/version2/main.sh
+++ scripts/version2/main.sh
@@ -10,3 +10,6 @@ sh ./migrate_jobs.sh
 cd "$(dirname "$0")"
 echo "Update Audio Files"
 sh ./update-audio-files.sh
+cd "$(dirname "$0")"
+echo "Caching Audio files"
+sh ../cache.sh /home/tamasuper/deploy/TAMA-Web-Audio KK3e4ce46d72c6bebe803d53be3e48947c
